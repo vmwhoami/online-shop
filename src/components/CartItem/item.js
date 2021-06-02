8e9b3c3db8c8d7f2c46f34aa2bec0a286b0878fb
Vitalie Melnic <vmwhoami@gmail.com>
2021-06-02 21:22:09 +0300

    Rename module components

--- src/components/CartItem/item.js
+++ src/components/CartItem/item.js
@@ -1,28 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Row, Col } from 'react-bootstrap';
+// import style from './item.module.css';
 // import PropTypes from 'prop-types';
 
 const Item = () => (
-  <div className="cart-item">
+  <div className="cartItem">
     <Row className="align-items-center">
       <Col className="col-12 col-lg-6">
-        <div className="media media-product">
+        <div className="media mediaProduct">
           <Link to="/products">
             <img src="" alt="" />
           </Link>
-          <div className="media-body">
-            <h5 className="media-title">Analog Magazine Rack</h5>
+          <div className="mediaBody">
+            <h5 className="mediaTitle">Analog Magazine Rack</h5>
             <span className="small">Red</span>
           </div>
           <Col className="col-4 col-lg-2 text-center">
-            <span className="cart-item-price">$120</span>
+            <span className="cartItemPrice">$120</span>
           </Col>
           <Col className="col-4 col-lg-2 text-center">
             <div className="counter">
-              <span className="counter-minus icon-minus" />
-              <input type="text" name="qty-1" className="counter-value" value="2" min="1" max="10" />
-              <span className="counter-plus icon-plus" />
+              <span className="counterMinus iconMinus" />
+              <input type="text" name="qty-1" className="counterValue" value="2" min="1" max="10" />
+              <span className="counterPlus iconPlus" />
             </div>
             <Col className="col-4 col-lg-2 text-center">
               <span className="cart-item-price">$240</span>

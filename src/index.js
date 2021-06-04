8383aa36c6a68a06313e4b95516f5006938e9f56
Vitalie Melnic <vmwhoami@gmail.com>
2021-06-04 20:47:19 +0300

    Refactor code for scss

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
-import './styles/main.css';
+import './styles/main.scss';
 import 'bootstrap/dist/css/bootstrap.css';
 import reportWebVitals from './reportWebVitals';
 // import fire from './firebase/firebaseConfig';

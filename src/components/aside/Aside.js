032bbd43693f341f46f2122f57b958cb9c7934c7
Vitalie Melnic <vmwhoami@gmail.com>
2021-06-06 10:29:59 +0200

    Add the products sidebar

---
+++ src/components/aside/Aside.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Col } from 'react-bootstrap';
+
+const Aside = () => (
+  <Col lg={3} className="sidebar">
+    <h2>This is my sidebar</h2>
+  </Col>
+);
+export default Aside;
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 import ProdDetails from '../components/Products/ProdDetail';
 import Layout from '../layout';
 import BreadCrubs from '../components/UIcomp/BreadCrubs';
 import CategoryTitle from '../components/UIcomp/CategoryTitle';
+import Aside from '../components/aside/Aside';
 
 // import PropTypes from 'prop-types';
 
@@ -11,8 +12,11 @@ const Products = () => (
   <Layout>
     <Container>
       <BreadCrubs first="Products" />
-      <CategoryTitle category="Yifu" quantity={10} />
-      <ProdDetails />
+      <CategoryTitle category="Men" quantity={10} />
+      <Row className="gutter-4">
+        <Aside />
+        <ProdDetails />
+      </Row>
     </Container>
   </Layout>
 );

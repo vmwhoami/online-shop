98fa0fe0b8a6bc8cb561a7a18211b746343d6fd8
Vitalie Melnic <vmwhoami@gmail.com>
2021-06-06 15:53:11 +0200

    Display number of products

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,25 +1,31 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Container, Row } from 'react-bootstrap';
 import ProdDetails from '../components/Products/ProdDetail';
 import Layout from '../layout';
 import BreadCrubs from '../components/UIcomp/BreadCrubs';
 import CategoryTitle from '../components/UIcomp/CategoryTitle';
 import Aside from '../components/aside/Aside';
-
+import destructureObjs from '../utils/destructureObj';
 // import PropTypes from 'prop-types';
 
-const Products = () => (
-  <Layout>
-    <Container>
-      <BreadCrubs first="Products" />
-      <CategoryTitle category="Men" quantity={10} />
-      <Row className="gutter-4">
-        <Aside />
-        <ProdDetails />
-      </Row>
-    </Container>
-  </Layout>
-);
+const Products = () => {
+  const prod = useSelector((state) => state.productsReducer.products?.data.products);
+  let products;
+  if (prod) products = destructureObjs(prod).length;
+  return (
+    <Layout>
+      <Container>
+        <BreadCrubs first="Products" />
+        <CategoryTitle category="Men" quantity={products} />
+        <Row className="gutter-4">
+          <Aside />
+          <ProdDetails />
+        </Row>
+      </Container>
+    </Layout>
+  );
+};
 
 // Products.propTypes = {
 

e4689ba8ad36c4fcaa4162aa0f8e08c5b678f1f3
Vitalie Melnic <vmwhoami@gmail.com>
2021-06-07 13:28:42 +0200

    Style the products Item

--- src/components/Products/ProdItem.js
+++ src/components/Products/ProdItem.js
@@ -18,27 +18,32 @@ const ProdItem = ({
           <ProdImages photo={photo} />
         </Link>
         <div className="product-meta">
-          <h3 className="product-title">
+          <h3 className="product-title font-weight-normal text-uppercase">
             <Link to={`/product/${id}`}>
               {name}
             </Link>
           </h3>
           <div className="product-container d-flex flex-column">
-            <span className="product-category">
-              {category.name}
-              /
-              {parent.name}
-            </span>
-            <span className="product-price">
-              {price}
-              $
-            </span>
-            <span className="product-quantity">
+            <div className="d-flex w-100 justify-content-between">
+              <span className="product-category font-weight-light  ">
+                {category.name}
+                /
+                {parent.name}
+              </span>
+
+              <span className="product-price font-weight-light text-right">
+
+                {' '}
+                {price}
+                $
+              </span>
+            </div>
+            <span className="product-quantity font-weight-light">
               {quantity}
               {' '}
-              Left
+              left
             </span>
-            <Button className="product-action">
+            <Button className="product-action rounded-0 text-uppercase py-3 text-dark">
               Add to cart
             </Button>
           </div>

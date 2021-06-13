4892cc21090dbbd13a7604c07bb567e5b44af61a
vmwhoami <vmwhoami@gmail.com>
2021-06-13 22:54:28 +0200

    Add axios request in layout component

--- src/layout.js
+++ src/layout.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Container } from 'react-bootstrap';
+import axios from 'axios';
 import NavBar from './components/Navbar/NavBar';
 import Footer from './components/Footer/Footer';
 import Cart from './components/Cart/Cart';
@@ -11,6 +12,11 @@ import RegAuth from './components/auth/RegAuth';
 const Layout = ({ children, mainpage }) => {
   const dispatch = useDispatch();
 
+  const getSmth = async () => {
+    const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=${process.env.REACT_APP_API_KEY}`;
+    axios.post(url, { token: 'ya29.c.KqABAwhAGaXNShqMuHLTHixBQut0niKJkH0BRAwq_dA226tfP8nAzxt1Qw81aAQbMk7Folm6wK8B6DVO5hPi0Cyozdi-OKKYHW_OucPPo1bi6cD7Ox6yCJ3JlQV_IoJGb_-KG2072edOJHJyMMddW0WGGfRXWT6uRTUFKU6VxJ3DYsOMPsQ8rLxiXAkE8wXtVZvgibUsBCdLI7G3xviepeIDRA', returnSecureToken: true });
+  };
+  getSmth().then((res) => console.log(res)).catch((err) => console.log(err));
   useEffect(() => {
     dispatch(getProducts());
   }, []);
